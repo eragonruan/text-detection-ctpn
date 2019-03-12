3809a20425818fd9c4bbd2425bfead0a2b9dcaa2
piginzoo <piginzoo@gmail.com>
2019-03-12 18:32:27 +0800

    update

--- main/train.py
+++ main/train.py
@@ -11,18 +11,18 @@ from nets import model_train as model
 from utils.dataset import data_provider as data_provider
 
 tf.app.flags.DEFINE_float('learning_rate', 1e-5, '') #学习率
-tf.app.flags.DEFINE_integer('max_steps', 50000, '') #？？？
+tf.app.flags.DEFINE_integer('max_steps', 4000, '') #我靠，人家原来是50000的设置
 tf.app.flags.DEFINE_integer('decay_steps', 30000, '')#？？？
 tf.app.flags.DEFINE_float('decay_rate', 0.1, '')#？？？
 tf.app.flags.DEFINE_float('moving_average_decay', 0.997, '')#、、、
 tf.app.flags.DEFINE_integer('num_readers', 4, '')#同时启动的进程4个
-tf.app.flags.DEFINE_string('gpu', '0', '')#使用CPU还是GPU
+tf.app.flags.DEFINE_string('gpu', '1', '') #使用第#1个GPU
 tf.app.flags.DEFINE_string('checkpoint_path', 'checkpoints/', '')
 tf.app.flags.DEFINE_string('logs_path', 'logs_mlt/', '')
 tf.app.flags.DEFINE_string('pretrained_model_path', 'data/vgg_16.ckpt', '')#VGG16的预训练好的模型，这个是直接拿来用的
 tf.app.flags.DEFINE_boolean('restore', False, '')
 tf.app.flags.DEFINE_boolean('debug_mode', False, '')
-tf.app.flags.DEFINE_integer('save_checkpoint_steps', 2000, '')
+tf.app.flags.DEFINE_integer('save_checkpoint_steps', 200, '')
 FLAGS = tf.app.flags.FLAGS
 
 tf.logging.set_verbosity(tf.logging.DEBUG)
