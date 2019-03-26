f7dc265497c06f572c4bda2e5855cdfafe325011
piginzoo <piginzoo@gmail.com>
2019-03-26 20:18:15 +0800

    update

--- bin/train.sh
+++ bin/train.sh
@@ -3,10 +3,14 @@ python main/train.py \
 	--pretrained_model_path=data/vgg_16.ckpt \
 	--max_steps=40000 \
 	--decay_steps=2000 \
-	--evaluate_steps=1 \
+	--evaluate_steps=10 \
 	--learning_rate=0.001 \
 	--save_checkpoint_steps=2000 \
 	--decay_rate=0.5 \
+	--debug_mode=False \
+	--logs_path=logs \
+	--moving_average_decay=0.997 \
+	--restore=False \
     >> ./logs_mlt/ctpn.log 2>&1
 
 
