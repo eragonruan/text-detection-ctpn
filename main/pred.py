43f00ef0ab246b4fad448a1e5227769f5b0f3178
piginzoo <piginzoo@gmail.com>
2019-05-23 10:46:39 +0800

    adjust hyper-parameters

--- main/pred.py
+++ main/pred.py
@@ -343,8 +343,8 @@ def predict_by_network(session, t_bbox_pred, t_cls_prob, t_input_im_info, t_inpu
     # cls_prob_val的shape: (1, H, W, Ax2)，所以需要先reshape成 (1, H, W, A, 2),然后去掉前景的概率值
     cls_prob_for_debug = cls_prob_val.reshape(1,
                                               cls_prob_val.shape[1],  # H
-                                              cls_prob_val.shape[2] // 10,  # W
-                                              10,  # 每个点上扩展的10个anchors
+                                              cls_prob_val.shape[2] // Config.NETWORK_ANCHOR_NUM,  # W
+                                              Config.NETWORK_ANCHOR_NUM,  # 每个点上扩展的10个anchors
                                               -1)  # <---2个值, 0:背景概率 1:前景概率
     _stat = stat(cls_prob_for_debug[:, :, :, :, 1].reshape(-1, 1))  # 去掉背景，只保留前景，然后传入统计
     logger.debug("前景返回概率情况:%s", _stat)
